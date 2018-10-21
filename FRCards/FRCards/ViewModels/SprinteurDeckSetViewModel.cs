5fa5c5f4e02699f636207f1319c82bac95764f15
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-21 23:35:05 +0200

    Adding SprinteurDeckSetViewModel

---
+++ FRCards/FRCards/ViewModels/SprinteurDeckSetViewModel.cs
@@ -0,0 +1,43 @@
+﻿using FRCards.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRCards.ViewModels
+{
+    public class SprinteurDeckSetViewModel : DeckSetViewModel
+    {
+        public override Card GetExhaustionCard()
+        {
+            return new Card()
+            {
+                Movement = 2,
+                Rider = RiderType.Sprinteur,
+                IsExhaustion = true
+            };
+        }
+
+        public SprinteurDeckSetViewModel()
+        {
+            UsedCards = new DeckViewModel();
+            ActiveDeck = new DeckViewModel();
+            Discarded = new DeckViewModel();
+            SelectionCards = null;
+            SelectedCard = null;
+
+            for (int count = 0; count < 3; count++)
+                ActiveDeck.Model.Cards.Push(new Card { Movement = 2, Rider = RiderType.Sprinteur, IsExhaustion = false });
+            for (int count = 0; count < 3; count++)
+                ActiveDeck.Model.Cards.Push(new Card { Movement = 3, Rider = RiderType.Sprinteur, IsExhaustion = false });
+            for (int count = 0; count < 3; count++)
+                ActiveDeck.Model.Cards.Push(new Card { Movement = 4, Rider = RiderType.Sprinteur, IsExhaustion = false });
+            for (int count = 0; count < 3; count++)
+                ActiveDeck.Model.Cards.Push(new Card { Movement = 5, Rider = RiderType.Sprinteur, IsExhaustion = false });
+            for (int count = 0; count < 3; count++)
+                ActiveDeck.Model.Cards.Push(new Card { Movement = 9, Rider = RiderType.Sprinteur, IsExhaustion = false });
+
+            ActiveDeck.Shuffle();
+        }
+
+    }
+}
