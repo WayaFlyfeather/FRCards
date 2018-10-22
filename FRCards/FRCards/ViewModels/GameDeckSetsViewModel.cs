904e8bbc0841e261f5e2576003eee71588153ae5
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-22 07:35:08 +0200

    Adding GameDeckSetsViewModel.

---
+++ FRCards/FRCards/ViewModels/GameDeckSetsViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRCards.ViewModels
+{
+    public class GameDeckSetsViewModel : BindableBase
+    {
+        private RouleurDeckSetViewModel rouleurSet;
+        public RouleurDeckSetViewModel RouleurSet
+        {
+            get => rouleurSet;
+            set => SetProperty(ref rouleurSet, value);
+        }
+
+        private SprinteurDeckSetViewModel sprinteurSet;
+        public SprinteurDeckSetViewModel SprinteurSet
+        {
+            get => sprinteurSet;
+            set => SetProperty(ref sprinteurSet, value);
+        }
+
+        public GameDeckSetsViewModel()
+        {
+            rouleurSet = new RouleurDeckSetViewModel();
+            rouleurSet.CanDrawCards = true;
+            rouleurSet.PropertyChanged += RouleurSet_PropertyChanged;
+
+            sprinteurSet = new SprinteurDeckSetViewModel();
+            sprinteurSet.CanDrawCards = true;
+            sprinteurSet.PropertyChanged += SprinteurSet_PropertyChanged;
+        }
+
+        private void SprinteurSet_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void RouleurSet_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
