fbf9adeecac959d027685ece6786f1c833c1ce88
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-24 23:28:56 +0200

    Adding DesignTime ViewModel

---
+++ FRCards/FRCards/ViewModels/DesignTimeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FRCards.ViewModels
+{
+    public static class DesignTimeViewModels
+    {
+        static RouleurDeckSetViewModel designRouleurDeckSetViewModel = null;
+        public static RouleurDeckSetViewModel DesignRouleurDeckSetViewModel => designRouleurDeckSetViewModel ?? (designRouleurDeckSetViewModel = new RouleurDeckSetViewModel());
+    }
+}
