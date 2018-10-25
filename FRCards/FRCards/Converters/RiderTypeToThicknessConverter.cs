f44987dd03f7c62bf77a3c2c58f77f9b86d77647
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-25 16:15:04 +0200

    Adding a RiderTypeToThicknessConverter (for nice frames around cards).

---
+++ FRCards/FRCards/Converters/RiderTypeToThicknessConverter.cs
@@ -0,0 +1,27 @@
+﻿using FRCards.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FRCards.Converters
+{
+    class RiderTypeToThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            RiderType rider = (RiderType)value;
+
+            if (rider == RiderType.Rouleur)
+                return new Thickness(6, 6, 4, 6);
+            else
+                return new Thickness(4, 6, 6, 6);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
