434467c40643b6dabfb37577a72426d260206229
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-26 21:01:40 +0200

    Adding DeckCardCountToThicknessConverter

---
+++ FRCards/FRCards/Converters/DeckCardCountToThicknessConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FRCards.Converters
+{
+    public class DeckCardCountToThicknessConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int cardCount = System.Convert.ToInt32(value);
+
+            return new Thickness(0, 0, cardCount, cardCount);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
