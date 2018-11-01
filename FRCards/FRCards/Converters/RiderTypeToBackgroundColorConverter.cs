0280430dacb7e7c4d93804e5714ef7d4feb196da
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-11-01 18:43:07 +0100

    Adding RtideTypeToBackgroundColorConverter

---
+++ FRCards/FRCards/Converters/RiderTypeToBackgroundColorConverter.cs
@@ -0,0 +1,34 @@
+﻿using FRCards.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace FRCards.Converters
+{
+    public class RiderTypeToBackgroundColorConverter : IValueConverter
+    {
+        static Color RouleurColor { get; } = new Color(0.8, 0.2, 0.2, 0.2);
+        static Color SprinteurColor { get; } = new Color(0.2, 0.2, 0.8, 0.2);
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+
+            if (value == null)
+                return Color.White;
+
+            RiderType rider = (RiderType)value;
+
+            if (rider == RiderType.Rouleur)
+                return RouleurColor;
+            else
+                return SprinteurColor;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
